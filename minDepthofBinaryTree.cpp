e737b3aa3d084f32e55a9c29254c10c4404ad6bc
shresthh <sumuandmishra@gmail.com>
2020-06-06 18:09:29 +0530

    this code does not contain the main function

---
+++ minDepthofBinaryTree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::minDepth(TreeNode* A) {
+    if(A==NULL){
+        return NULL;
+    }
+    if(A->left==NULL &&  A->right==NULL){
+        return 1;
+    }
+    if(!A->left){
+        return 1+minDepth(A->right);
+    
+    }
+    if(!A->right){
+        return 1+minDepth(A->left);
+    }
+    return 1+ (min(minDepth(A->left),minDepth(A->right)));
+}
