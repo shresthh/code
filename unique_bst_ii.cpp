fa3afa01e21a629069f95595d4ea30bdf62ae260
shresthh <sumuandmishra@gmail.com>
2020-06-25 21:26:19 +0530

    Dynamic Programming

---
+++ unique_bst_ii.cpp
@@ -0,0 +1,14 @@
+int catalan_number(int n){
+    int catalan[n+1];
+    catalan[0] = catalan[1] = 1;
+    for (int i=2;i<=n;i++){
+        catalan[i] = 0;
+        for (int j=0;j<i;j++){
+            catalan[i] += catalan[j] *catalan[i-j-1];
+        }
+    }
+    return catalan[n];
+}
+int Solution::numTrees(int A) {
+    return catalan_number(A);
+}
