3030904eab105c1f12f809b27be13d426c5160b7
shresthh <sumuandmishra@gmail.com>
2020-07-24 17:17:53 +0530

    Array

---
+++ rotate_matrix.cpp
@@ -0,0 +1,18 @@
+void Solution::rotate(vector<vector<int> > &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    int n = A.size();
+    int m = A[0].size();
+    for (int i = 0; i< n ; i++){
+        for(int j =i ; j<m ; j++){
+            swap(A[i][j], A[j][i]);
+        }
+    }
+    for(int i =0; i< n; i++){
+        reverse(A[i].begin(),A[i].end());
+    }
+}
+
