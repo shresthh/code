0788fd8766a1340b5a0d1b2589b5597f32f6b537
shresthh <sumuandmishra@gmail.com>
2020-07-29 10:47:32 +0530

    Maths

---
+++ FizzBuzz.cpp
@@ -0,0 +1,14 @@
+vector<string> Solution::fizzBuzz(int A) {
+    vector<string > B;
+    for (int i = 1; i<= A; i++){
+        if(i%3==0 && i%5 == 0)
+            B.push_back("FizzBuzz");
+        else if(i%3 == 0)
+            B.push_back("Fizz");
+        else if(i%5 == 0)
+            B.push_back("Buzz");
+        else
+            B.push_back(to_string(i));
+    }
+    return B;
+}
